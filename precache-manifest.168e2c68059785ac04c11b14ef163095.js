e8e8bf48790f2a1af9040cd0eb8f20bb94e2dfce
Daniil Loban <daniil.loban@gmail.com>
2020-02-07 02:15:35 +0300

    first

---
+++ precache-manifest.168e2c68059785ac04c11b14ef163095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69c72532adc5cbe8dc9f680fa29a2cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "036dd71fdd3db5824c44",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66f63e1c8b766fcc4d2f",
+    "url": "./static/js/2.a85994ce.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.a85994ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "036dd71fdd3db5824c44",
+    "url": "./static/js/main.46c7f0c6.chunk.js"
+  },
+  {
+    "revision": "3859e7bba3c6c17a2615",
+    "url": "./static/js/runtime-main.b14b29b4.js"
+  }
+]);
